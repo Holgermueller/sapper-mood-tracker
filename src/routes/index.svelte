01b3ad547dc272b803589c14bfe20c8842c4193b
Holger Mueller <9jholger@gmail.com>
2020-10-18 12:18:22 -0500

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,47 @@
+<script>
+  import successkid from "images/successkid.jpg";
+</script>
+
+<style>
+  h1,
+  figure,
+  p {
+    text-align: center;
+    margin: 0 auto;
+  }
+
+  h1 {
+    font-size: 2.8em;
+    text-transform: uppercase;
+    font-weight: 700;
+    margin: 0 0 0.5em 0;
+  }
+
+  figure {
+    margin: 0 0 1em 0;
+  }
+
+  img {
+    width: 100%;
+    max-width: 400px;
+    margin: 0 0 1em 0;
+  }
+
+  p {
+    margin: 1em auto;
+  }
+
+  @media (min-width: 480px) {
+    h1 {
+      font-size: 4em;
+    }
+  }
+</style>
+
+<svelte:head>
+  <title>Mood Tracker</title>
+</svelte:head>
+
+<h1>Welcome to the Mood Tracker!</h1>
+
+<p><strong>Keep track of your mood swings here!!</strong></p>
