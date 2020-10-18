e54d1ed4c4424e6187792f63855ff94918287d06
Holger Mueller <9jholger@gmail.com>
2020-10-18 12:28:00 -0500

    added footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,19 @@
+<style>
+  footer {
+    background: #f7f7f7;
+    padding: 5px;
+    bottom: 0;
+    position: fixed;
+    width: 100%;
+  }
+  div {
+    margin: 0;
+    text-align: center;
+    color: #ff3e00;
+    text-transform: capitalize;
+  }
+</style>
+
+<footer>
+  <div class="copyright">&copy; 2020 Holger Mueller</div>
+</footer>
