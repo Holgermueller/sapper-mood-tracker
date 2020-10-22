4f69362bb99670a49992bf95771708489ca89f51
Holger Mueller <9jholger@gmail.com>
2020-10-22 13:26:40 -0500

    added 404 page

---
+++ src/routes/404.svelte
@@ -0,0 +1,22 @@
+<style>
+  .not-found {
+    margin: 3rem;
+  }
+  a {
+    text-decoration: none;
+    color: #ff3e00;
+  }
+</style>
+
+<svelte:head>
+  <title>404</title>
+</svelte:head>
+
+<section class="not-found">
+  <h1>Page not found!</h1>
+  <h3>You must have gotten bad directions from someone!</h3>
+  <h4>
+    Here, let's help you get back to where you started:
+    <a href="/">Home</a>
+  </h4>
+</section>
